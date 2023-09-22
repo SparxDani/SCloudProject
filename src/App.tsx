dcd101fe26dda6be150a2f0e6429d6797eadeaa7
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-09-22 11:59:05 -0500

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainHeader from "./common/MainHeader.tsx";
 import MainFooter from "./common/MainFooter.tsx";
-import MainBanner from "./common/MainBanner.tsx";
 import MainNav from "./common/MainNav.tsx";
 import Home from "./pages/Home.tsx";
 import Game from "./pages/Game.tsx";
@@ -13,14 +12,12 @@ function App() {
         <>
             <BrowserRouter>
                 <MainHeader/>
-                <MainFooter/>
-                <MainBanner/>
                 <MainNav/>
                 <Routes>
                     <Route path='/' element={<Home/>}/>
                     <Route path='/game' element={<Game/>}/>
                 </Routes>
-                <MainFooter></MainFooter>
+                <MainFooter/>
             </BrowserRouter>
         </>
     );
